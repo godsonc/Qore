7f206765a6f2875f04c2065d5cc1d8a8cabc0ca4
Godson C. <nwankwogodsonc@gmail.com>
2024-02-22 15:56:12 +0100

    url

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
 
-    baseUrl: '',
+    baseUrl: 'https://qore.enyata.com',
     viewportHeight: 1000,
     defaultCommandTimeout: 3000,
     viewportWidth: 1500,
