a26c29b459151a45deeb0a35e26c56b7900a5ad6
Godson C. <nwankwogodsonc@gmail.com>
2024-03-06 10:25:22 +0100

    login

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -1,3 +1,11 @@
-it('Qore', () => {
+it('gabbi health', () => {
     cy.visit('/')
-}) 
+    
+    cy.get('[data-testid="test-emailAddress"]')
+       .type('chinedu@enyata.com')
+
+    cy.get('[data-testid="test-password"]')
+      .type('P@ssword01')
+      
+       
+})
